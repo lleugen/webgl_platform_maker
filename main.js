d2ca3600544311ddc98891fab310f1f01a3d3ee4
Eugenio <eugen951@gmail.com>
2021-06-11 10:36:48 +0200

    test vcs

--- main.js
+++ main.js
@@ -9,7 +9,17 @@
 //     aspectRatio = canvas.clientWidth/canvas.clientHeight;
 
 // }
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-context.fillStyle = "#FF0000";
-context.fillRect(0,0,150,75);
+
+function main(){
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    context.fillStyle = "#FF0000";
+    context.fillRect(0,0,150,75);
+
+    ghost_mesh = new Object.mesh("./assets/ghost.obj")
+}
+
+
+async function load_object(){
+    const response = await fetch("./assets/ghost.obj");
+}
