0e2918c659da7ad297d0e38364c5c4cc5b9e4643
Eugenio <eugen951@gmail.com>
2021-07-05 16:18:01 +0200

    unite button_functions, gui_functions into input_events

--- button_functions.js
+++
@@ -1,7 +0,0 @@
-function addTree(){
-    let x = Math.random() * 100 - 50;
-    let z = Math.random() * 100 - 50;
-    let name = 'tree_'+renderer.objects.length;
-    renderer.addObject(name, 'tree', [x,0,z], [0,0,0]);
-    console.log(renderer.objects)
-}
--- input_events.js
+++ input_events.js
@@ -113,6 +113,82 @@ function doMouseMove(event) {
 }
 
 
+function addTree(){
+    let x = Math.random() * 100 - 50;
+    let z = Math.random() * 100 - 50;
+    let name = 'tree_'+renderer.objects.length;
+    renderer.addObject(name, 'tree', [x,0,z], [0,0,0]);
+    console.log(renderer.objects)
+}
+
+
+var sliderValuex = 1;
+function onSliderChangex(value){
+    console.log("Slider value changed to "+value);
+    sliderValuex = value;
+    renderer.updateObject(focusedObjectName);
+}
+var sliderValuey = 1;
+function onSliderChangey(value){
+    console.log("Slider value changed to "+value);
+    sliderValuey = value;
+    renderer.updateObject(focusedObjectName);
+}
+var sliderValuez = 1;
+function onSliderChangez(value){
+    console.log("Slider value changed to "+value);
+    sliderValuez = value;
+    renderer.updateObject(focusedObjectName);
+}
+var nearPlane = 1;
+function onSliderChangeNear(value){
+    console.log("Slider value changed to "+value);
+    nearPlane = parseFloat(value);
+}
+var farPlane = 300;
+function onSliderChangeFar(value){
+    console.log("Slider value changed to "+value);
+    farPlane = parseFloat(value);
+}
+var cameraWindowWidth = 10;
+function onSliderChangew(value){
+    console.log("Slider value changed to "+value);
+    cameraWindowWidth = value;
+}
+var cameraElevation = 0;
+function onSliderChangeElevation(value){
+    console.log("Slider value changed to "+value);
+    cameraElevation = value;
+}
+var cameraAngle = 0;
+function onSliderChangeAngle(value){
+    console.log("Slider value changed to "+value);
+    cameraAngle = value;
+}
+
+var projectionType = "perspective";
+function onRadioButtonChange(value){
+  console.log("Radio button value changed to "+value);
+  projectionType = value;
+}
+
+var worldAnglex = 0;
+function onSliderChangeWorldAnglex(value){
+    console.log("Slider value changed to "+value);
+    worldAnglex = value;
+}
+var worldAngley = 0;
+function onSliderChangeWorldAngley(value){
+    console.log("Slider value changed to "+value);
+    worldAngley = value;
+}
+var worldAnglez = 0;
+function onSliderChangeWorldAnglez(value){
+    console.log("Slider value changed to "+value);
+    worldAnglez = value;
+}
+
+
 function raycast(x, y){
 	let invProjection;
 	let cameraSpaceRay, worldSpaceRay;
